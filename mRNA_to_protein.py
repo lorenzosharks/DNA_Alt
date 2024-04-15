ed217fd95f492984d98aec89b500e123fcc5cef3
lorenzosharks <161920089+lorenzosharks@users.noreply.github.com>
2024-04-14 21:38:38 -0700

    fffffffffffffffffffffff

--- mRNA_to_protein.py
+++ mRNA_to_protein.py
@@ -1,3 +1,5 @@
+import sys
+
 #Function
 def del_early(string, index):
     return string[index:]
@@ -5,18 +7,14 @@ def del_early(string, index):
 def mRNA_list_creation(mid):
     return [mid[i:i+3] for i in range(0, len(mid), 3)]
 
-def first_combination(combinations, strings_list):
-    for combination in combinations:
-        for i, string in enumerate(strings_list):
-            index = string.find(combination)
-            if index != -1:
-                return string, i, index
-    return None, -1, -1
-
 mRNA=input("Insert your mRNA strand (After splicing and whatnot):")
 
 #Finding the first start codon
 mRNA_index=mRNA.find("AUG")
+
+if mRNA_index == -1:
+    print("This chain has no start point.")
+    sys.exit()
 
 #Splicing the string
 mRNA_mid=del_early(mRNA, mRNA_index)
@@ -26,6 +24,18 @@ mRNA_list=mRNA_list_creation(mRNA_mid)
 
 ending_codon=["UGA", "UAG", "UAA"]
 
-ending_index=first_combination(ending_codon, mRNA_list)
+#finding the ending codon
+
+ending_index = -1
+
+for i in range(len(mRNA_list)):
+    y=mRNA_list[i]
+    for x in range(3):
+        if y=="UAA" or y=="UAG" or y=="UGA":
+            ending_index=i
+            break
+    if ending_index !=-1:
+        break
 
 print(ending_index)
+print(mRNA_list[ending_index])
